d34de3a5581ffbbb8743d1d7f83b3d3952faec37
dijitaltup <harunemirhan826@gmail.com>
2025-12-09 15:39:57 +0300

    feat(channels): Finalize channels layer by exporting patient bot and dentist panel runners

---
+++ src/dent_bot/channels/__init__.py
@@ -0,0 +1,7 @@
+from .telegram import run_telegram_bot
+from .dentist_panel import run_dentist_panel
+
+__all__ = [
+    "run_telegram_bot", # Hasta Botu
+    "run_dentist_panel", # Doktor Paneli
+]
